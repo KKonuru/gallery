6873ecfcf47ac2fd9b8cb29bbe04f35f59e42366
KKonuru <kausthubhkonuru@gmail.com>
2024-06-19 12:35:59 -0400

    Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/1d7feb4a-530b-4d9b-b698-436e6bebe468-i9ujm1.webp",
+  "https://utfs.io/f/35453f08-a9d3-4c0a-b082-f6fd258dce94-yu13ay.webp",
+  "https://utfs.io/f/6d5dcce1-13a3-42a1-87e7-80f2e61bc886-7xkir8.avif",
+  "https://utfs.io/f/63f5e6a9-43cc-4ea0-a41d-34328e023e97-vwaf5h.avif",
+
+];
+
+
+const mockImages = mockUrls.map((url,index) => ({
+    id:index+1,
+    url:url,
+  }
+));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      I made some changes so that this is can be seen in the git changes.
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages,...mockImages,...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+        ))  }
+      </div>
     </main>
   );
 }
