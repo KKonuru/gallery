92425b7376ee00eb5005e8a1ac2d0fb824e48c2e
KKonuru <kausthubhkonuru@gmail.com>
2024-06-29 15:27:30 -0400

    Linked database to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,19 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 export const dynamic="force-dynamic";
-const mockUrls = [
-  "https://utfs.io/f/1d7feb4a-530b-4d9b-b698-436e6bebe468-i9ujm1.webp",
-  "https://utfs.io/f/35453f08-a9d3-4c0a-b082-f6fd258dce94-yu13ay.webp",
-  "https://utfs.io/f/6d5dcce1-13a3-42a1-87e7-80f2e61bc886-7xkir8.avif",
-  "https://utfs.io/f/63f5e6a9-43cc-4ea0-a41d-34328e023e97-vwaf5h.avif",
-
-];
-
-
-const mockImages = mockUrls.map((url,index) => ({
-    id:index+1,
-    url:url,
-  }
-));
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.image.findMany({
+    orderBy:(model, {desc}) => desc(model.id),
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {
-          posts.map((post) => (
-            <div key={post.id} className="w-48">
-              {post.name}
-            </div>
-          ))
-        }
-        {[...mockImages,...mockImages,...mockImages].map((image,index) => (
-          <div key={image.id+"-"+index} className="w-48">
+        {[...images,...images,...images].map((image,index) => (
+          <div key={image.id+"-"+index} className="flex w-48 flex-col">
             <img src={image.url}/>
+            <div>{image.name}</div>
           </div>
         ))  }
       </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,11 +19,12 @@ import { type AdapterAccount } from "next-auth/adapters";
  */
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
-export const posts = createTable(
-  "post",
+export const image = createTable(
+  "image",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", {length:1024}).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
