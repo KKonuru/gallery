148f6229a9d5e2827c8cc16edb0c17cae52b3e62
KKonuru <kausthubhkonuru@gmail.com>
2024-07-27 12:57:27 -0400

    Fixed modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,14 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: {id: number}) {
     const image = await getImage(props.id);
-    return <img src={image.url} className="w-96"/>
+    return( 
+        <div className="flex w-full h-full">
+            <div className="flex-shrink flex justify-center">
+            <img src={image.url} className="object-contain flex-shrink"/>
+            </div>
+            <div className="w-48 flex-shrink-0 flex-col">
+                <div className="text-xl bold">{image.name}</div>
+            </div>
+        </div>
+    )
 }
