e988ed00a8dd359ed6ad793e5ece0cbf402b97cc
KKonuru <kausthubhkonuru@gmail.com>
2024-07-27 18:49:30 -0400

    Added delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,7 +1,10 @@
 import "server-only";
 import {db} from "./db";
 import {auth} from "@clerk/nextjs/server"; 
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
+import {image} from "./db/schema";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 export async function getMyImages(){
     const user = auth();
 
@@ -20,11 +23,21 @@ export async function getImage(id: number){
     const user = auth();
     if (!user.userId) throw new Error("Unauthorized");
 
-    const image = await db.query.image.findFirst({
+    const images = await db.query.image.findFirst({
         where: (model, {eq}) => eq(model.id,id),
     });
 
-    if (!image) throw new Error("Image not found");
-    if(image.userId !== user.userId) throw new Error("Unauthorized");
-    return image;
+    if (!images) throw new Error("Image not found");
+    if(images.userId !== user.userId) throw new Error("Unauthorized");
+    return images;
+}
+
+export async function deleteImage(id: number){
+
+    const user = auth();
+    if (!user.userId) throw new Error("Unauthorized");
+    await db.delete(image).where(and(eq(image.id,id),eq(image.userId,user.userId)));
+
+    revalidatePath("/");
+    redirect("/");
 }
